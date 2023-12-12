e78cc47bd5ace24cb8d03353f3d2176ffe588b17
nrll <naraa_togy@yahoo.com>
2023-12-12 17:40:30 +0800

    added budget dialog

--- frontend/src/components/sub/Budget.tsx
+++ frontend/src/components/sub/Budget.tsx
@@ -7,21 +7,59 @@ import {
   TextField,
   FormLabel,
   InputAdornment,
+  Select,
+  FormControl,
+  MenuItem,
 } from "@mui/material";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+interface OutType {
+  typeName: string;
+}
 
 export default function Budget(props: any): JSX.Element {
   const { open, onClose, ledger } = props;
   const [budgetAmount, setBudgetAmount] = useState<number>(0);
+  const [types, setTypes] = useState<OutType[]>([]);
+  const [budgetType, setBudgetType] = useState<string>("");
+
+  useEffect(() => {
+    getTypes();
+  }, []);
+
+  const getTypes = async (): Promise<any> => {
+    try {
+      const response = await fetch("http://localhost:8000/type", {
+        method: "get",
+        headers: { "Content-Type": "application/json" },
+      });
+      if (response.status === 200) {
+        const data = (await response.json()) as any;
+        const expenseTypes = data["expense"];
+        setTypes(() => [...expenseTypes]);
+      }
+    } catch (error) {
+      console.log("Error fetching type data", error);
+    }
+  };
+
+  const onTypeChange = (newEvent: any) => {
+    setBudgetType(() => newEvent.target.value);
+  };
 
   const onSubmit = async (event: any): Promise<any> => {
     event.preventDefault();
     const newBudget = {
       ab_id: ledger,
       b_amount: budgetAmount,
-      type: "Dining", //change later
+      type_name: budgetType,
     };
     console.log("newBudget", newBudget);
+    fetch("http://localhost:8000/budget", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newBudget),
+    });
   };
 
   return (
@@ -30,28 +68,47 @@ export default function Budget(props: any): JSX.Element {
 
       <DialogContent>
         <form onSubmit={onSubmit} className="add-form">
-          <FormLabel required>Budget Amount</FormLabel>
-          <TextField
-            type="number"
-            size="small"
-            id="budget_amount"
-            defaultValue={0}
-            onChange={(e) => setBudgetAmount(Number(e.target.value))}
-            fullWidth
-            InputProps={{
-              inputProps: { min: 0 },
-              endAdornment: <InputAdornment position="end">¥</InputAdornment>,
-            }}
-            sx={{
-              "& input::-webkit-outer-spin-button, & input::-webkit-inner-spin-button":
-                {
-                  display: "none",
+          <FormControl sx={{ width: 552 }}>
+            <FormLabel required>Budget Type</FormLabel>
+            <Select
+              value={budgetType}
+              onChange={onTypeChange}
+              fullWidth
+              size="small"
+              sx={{ mb: 2 }}
+            >
+              {types[0] &&
+                types.map((type, i) => (
+                  <MenuItem key={i} value={type.typeName}>
+                    {type.typeName}
+                  </MenuItem>
+                ))}
+            </Select>
+          </FormControl>
+          <FormControl sx={{ width: 552 }}>
+            <FormLabel required>Budget Amount</FormLabel>
+            <TextField
+              type="number"
+              size="small"
+              id="budget_amount"
+              defaultValue={0}
+              onChange={(e) => setBudgetAmount(Number(e.target.value))}
+              fullWidth
+              InputProps={{
+                inputProps: { min: 0 },
+                endAdornment: <InputAdornment position="end">¥</InputAdornment>,
+              }}
+              sx={{
+                "& input::-webkit-outer-spin-button, & input::-webkit-inner-spin-button":
+                  {
+                    display: "none",
+                  },
+                "& input[type=number]": {
+                  MozAppearance: "textfield",
                 },
-              "& input[type=number]": {
-                MozAppearance: "textfield",
-              },
-            }}
-          />
+              }}
+            />
+          </FormControl>
         </form>
       </DialogContent>
 
--- frontend/src/components/sub/Transaction.tsx
+++ frontend/src/components/sub/Transaction.tsx
@@ -1,203 +1,205 @@
 import {
-    Dialog,
-    DialogTitle,
-    DialogContent,
-    DialogActions,
-    Button,
-    TextField,
-    FormLabel,
-    InputAdornment,
-    Select,
-    MenuItem,
-    RadioGroup,
-    FormControlLabel,
-    Radio,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  Button,
+  TextField,
+  FormLabel,
+  InputAdornment,
+  Select,
+  MenuItem,
+  RadioGroup,
+  FormControlLabel,
+  Radio,
 } from "@mui/material";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import FormControl from "@mui/material/FormControl";
 
 //Temp dummy data
 const typesData = {
-    income: [
-        {
-            typeName: "Bonus",
-        },
-        {
-            typeName: "Other Income",
-        },
-        {
-            typeName: "Red bag",
-        },
-        {
-            typeName: "Salary",
-        },
-    ],
-    outcome: [
-        {
-            typeName: "Dining",
-        },
-        {
-            typeName: "Other Expenditure",
-        },
-        {
-            typeName: "Shopping",
-        },
-        {
-            typeName: "Sports",
-        },
-    ],
+  income: [
+    {
+      typeName: "Bonus",
+    },
+    {
+      typeName: "Other Income",
+    },
+    {
+      typeName: "Red bag",
+    },
+    {
+      typeName: "Salary",
+    },
+  ],
+  outcome: [
+    {
+      typeName: "Dining",
+    },
+    {
+      typeName: "Other Expenditure",
+    },
+    {
+      typeName: "Shopping",
+    },
+    {
+      typeName: "Sports",
+    },
+  ],
 };
 
 interface SelectedSub {
-    main: string;
-    sub: string;
+  main: string;
+  sub: string;
 }
 
 interface SubType {
-    typeName: string;
+  typeName: string;
 }
 
 export default function Transaction(props: any): JSX.Element {
-    const {open, onClose} = props;
-    const [amount, setAmount] = useState<number>(0);
-    const [types, setTypes] = useState<any>({}); //in-out all data
-    const [subTypes, setSubTypes] = useState<SubType[]>([]);
-    const [selectedType, setSelectedType] = useState<string>("income");
-    const [selectedSub, setSelectedSub] = useState<SelectedSub | "">("");
-    const [remark, setRemark] = useState<string>("");
+  const { open, onClose } = props;
+  const [amount, setAmount] = useState<number>(0);
+  const [types, setTypes] = useState<any>({}); //in-out all data
+  const [subTypes, setSubTypes] = useState<SubType[]>([]);
+  const [selectedType, setSelectedType] = useState<string>("income");
+  const [selectedSub, setSelectedSub] = useState<SelectedSub | "">(
+    {} as SelectedSub
+  );
+  const [remark, setRemark] = useState<string>("");
 
-    useEffect(() => {
-        getTypes();
-    }, []);
+  useEffect(() => {
+    getTypes();
+  }, []);
 
-    const getTypes = async (): Promise<any> => {
-        try {
-            const response = await fetch("http://localhost:8000/type", {
-                method: "get",
-                headers: {"Content-Type": "application/json"},
-            });
-            // response.json().then((res) => console.log("res", res));
+  const getTypes = async (): Promise<any> => {
+    try {
+      const response = await fetch("http://localhost:8000/type", {
+        method: "get",
+        headers: { "Content-Type": "application/json" },
+      });
+      // response.json().then((res) => console.log("res", res));
 
-            if (response.status === 200) {
-                const data = await response.json() as any;
-                console.log("types", data);
-                setTypes(data);
-                setSubTypes(() => [...data["income"]]);
-            }
-        } catch (error) {
-            console.log("Error fetching type data", error);
-        }
+      if (response.status === 200) {
+        const data = (await response.json()) as any;
+        setTypes(data);
+        const subData = data["income"];
+        setSubTypes(() => [...subData]);
+      }
+    } catch (error) {
+      console.log("Error fetching type data", error);
+    }
+  };
+
+  const onTypeChange = (event: any, value: any): void => {
+    setSelectedType(value);
+    if (value === "income") {
+      const inTypes = types.income;
+      setSubTypes(() => [...inTypes]);
+      setSelectedSub({ main: "income", sub: "" });
+    } else {
+      const outTypes = types.expense;
+      setSubTypes(() => [...outTypes]);
+      setSelectedSub({ main: "expense", sub: "" });
+    }
+  };
+
+  const onSubTypeChange = (event: any, value: any) => {
+    setSelectedSub({
+      main: selectedType,
+      sub: event.target.value,
+    });
+  };
+
+  const onSubmit = async (event: any): Promise<any> => {
+    event.preventDefault();
+
+    const newTransaction = {
+      l_amount: amount,
+      type_name: selectedSub.sub,
+      remark: remark,
+      a_id: 0, // TODO
+      ab_id: 0, // TODO
     };
 
-    const onTypeChange = (event: any, value: any): void => {
-        setSelectedType(value);
-        setSelectedSub("");
-        if (value === "income") {
-            setSubTypes(() => [...types["income"]]);
-        } else {
-            setSubTypes(() => [...types["expense"]]);
-        }
-    };
+    const response = await fetch("http://localhost:8000/log", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newTransaction),
+    });
+    console.log("response", response);
+  };
 
-    const onSubTypeChange = (event: any, value: any) => {
-        setSelectedSub({
-            main: selectedType,
-            sub: event.target.value,
-        });
-    };
+  return (
+    <Dialog open={open} onClose={onClose}>
+      <DialogTitle>新建日志</DialogTitle>
+      <DialogContent>
+        <form onSubmit={onSubmit}>
+          <FormLabel>Amount</FormLabel>
+          <TextField
+            required
+            type="number"
+            size="small"
+            id="trans_amount"
+            value={amount}
+            onChange={(e) => setAmount(+e.target.value)}
+            fullWidth
+            sx={{ mb: 2 }}
+            InputProps={{
+              inputProps: { min: 0 },
+              endAdornment: <InputAdornment position="end">¥</InputAdornment>,
+            }}
+          />
+          <br />
 
-    const onSubmit = async (event: any): Promise<any> => {
-        event.preventDefault();
+          <FormLabel>Type</FormLabel>
+          <RadioGroup value={selectedType} onChange={onTypeChange}>
+            <FormControlLabel
+              value="income"
+              control={<Radio />}
+              label="Income"
+            />
+            <FormControlLabel
+              value="expense"
+              control={<Radio />}
+              label="Expense"
+            />
+          </RadioGroup>
+          <br />
 
-        const newTransaction = {
-            l_amount: amount,
-            type_name: selectedSub.sub,
-            remark: remark,
-            a_id: 0, // TODO
-            ab_id: 0, // TODO
-        };
+          <FormControl fullWidth>
+            <FormLabel>Sub-Type</FormLabel>
+            <Select
+              value={selectedSub["sub"] || ""}
+              onChange={onSubTypeChange}
+              fullWidth
+              size="small"
+              sx={{ mb: 2 }}
+            >
+              {subTypes[0] &&
+                subTypes.map((subType, i) => (
+                  <MenuItem key={i} value={subType.typeName}>
+                    {subType.typeName}
+                  </MenuItem>
+                ))}
+            </Select>
+          </FormControl>
 
-        const response = await fetch("http://localhost:8000/log", {
-            method: "post",
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify(newTransaction),
-        });
-
-        console.log("response", response);
-    };
-
-    return (
-        <Dialog open={open} onClose={onClose}>
-            <DialogTitle>新建日志</DialogTitle>
-            <DialogContent>
-                <form onSubmit={onSubmit}>
-                    <FormLabel>Amount</FormLabel>
-                    <TextField
-                        required
-                        type="number"
-                        size="small"
-                        id="trans_amount"
-                        value={amount}
-                        onChange={(e) => setAmount(+e.target.value)}
-                        fullWidth
-                        sx={{mb: 2}}
-                        InputProps={{
-                            inputProps: {min: 0},
-                            endAdornment: <InputAdornment position="end">¥</InputAdornment>,
-                        }}
-                    />
-                    <br/>
-
-                    <FormLabel>Type</FormLabel>
-                    <RadioGroup value={selectedType} onChange={onTypeChange}>
-                        <FormControlLabel
-                            value="income"
-                            control={<Radio/>}
-                            label="Income"
-                        />
-                        <FormControlLabel
-                            value="outcome"
-                            control={<Radio/>}
-                            label="Expense"
-                        />
-                    </RadioGroup>
-                    <br/>
-
-                    <FormControl fullWidth>
-                        <FormLabel>Sub-Type</FormLabel>
-                        <Select
-                            value={selectedSub.sub || subTypes[0] || ""}
-                            onChange={onSubTypeChange}
-                            fullWidth
-                            size="small"
-                            sx={{mb: 2}}
-                        >
-                            {subTypes[0] &&
-                                subTypes.map((subType, i) => (
-                                    <MenuItem key={i} value={subType.typeName}>
-                                        {subType.typeName}
-                                    </MenuItem>
-                                ))}
-                        </Select>
-                    </FormControl>
-
-                    <FormLabel>Remark</FormLabel>
-                    <TextField
-                        fullWidth
-                        size="small"
-                        value={remark}
-                        onChange={(e) => setRemark(e.target.value)}
-                    />
-                </form>
-            </DialogContent>
-            <DialogActions>
-                <Button onClick={onClose}>Cancel</Button>
-                <Button onClick={onSubmit}>Add</Button>
-            </DialogActions>
-        </Dialog>
-    );
+          <FormLabel>Remark</FormLabel>
+          <TextField
+            fullWidth
+            size="small"
+            value={remark}
+            onChange={(e) => setRemark(e.target.value)}
+          />
+        </form>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onClose}>Cancel</Button>
+        <Button onClick={onSubmit}>Add</Button>
+      </DialogActions>
+    </Dialog>
+  );
 }
 
 //1. alert empty
-//2. type get
-//3, style = tab choice
